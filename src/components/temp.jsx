412ead5e10fa64512aa7264de84ba0d24705d7d6
DevBhuptani <190770116008@socet.edu.in>
2022-08-04 15:37:37 +0530

    master

--- src/components/temp.jsx
+++ src/components/temp.jsx
@@ -9,7 +9,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=4f3fe8415e7beeade9da42411b338612`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=4f3fe8415e7beeade9da42411b338612`; // Your own API key
 
       let res = await fetch(url);
       let data = await res.json();
